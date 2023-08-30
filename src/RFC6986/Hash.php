fe5ff5bde120b05273dd7ce60c9b63d6995cf14d
jyxjjj <773933146@qq.com>
2023-08-30 14:34:46 +0800

    Update Hash.php

--- src/RFC6986/Hash.php
+++ src/RFC6986/Hash.php
@@ -4,12 +4,22 @@ namespace DESMG\RFC6986;
 
 class Hash
 {
+    public static function sha256_equals(string $hash, string $data, string $key = ''): bool
+    {
+        return hash_equals($hash, self::sha256($data, $key));
+    }
+
     /**
      * @return string 64 characters long
      */
     public static function sha256(string $data, string $key = ''): string
     {
-        return strtoupper($key == '' ? hash('sha256', $data, false) : hash_hmac('sha256', $data, $key, false));
+        return strtoupper($key == '' ? hash('sha256', $data) : hash_hmac('sha256', $data, $key));
+    }
+
+    public static function sha256_file_equals(string $hash, string $data, string $key = ''): bool
+    {
+        return hash_equals($hash, self::sha256_file($data, $key));
     }
 
     /**
@@ -17,7 +27,12 @@ class Hash
      */
     public static function sha256_file(string $data, string $key = ''): string
     {
-        return strtoupper($key == '' ? hash_file('sha256', $data, false) : hash_hmac_file('sha256', $data, $key, false));
+        return strtoupper($key == '' ? hash_file('sha256', $data) : hash_hmac_file('sha256', $data, $key));
+    }
+
+    public static function sha384_equals(string $hash, string $data, string $key = ''): bool
+    {
+        return hash_equals($hash, self::sha384($data, $key));
     }
 
     /**
@@ -25,7 +40,12 @@ class Hash
      */
     public static function sha384(string $data, string $key = ''): string
     {
-        return strtoupper($key == '' ? hash('sha384', $data, false) : hash_hmac('sha384', $data, $key, false));
+        return strtoupper($key == '' ? hash('sha384', $data) : hash_hmac('sha384', $data, $key));
+    }
+
+    public static function sha384_file_equals(string $hash, string $data, string $key = ''): bool
+    {
+        return hash_equals($hash, self::sha384_file($data, $key));
     }
 
     /**
@@ -33,7 +53,12 @@ class Hash
      */
     public static function sha384_file(string $data, string $key = ''): string
     {
-        return strtoupper($key == '' ? hash_file('sha384', $data, false) : hash_hmac_file('sha384', $data, $key, false));
+        return strtoupper($key == '' ? hash_file('sha384', $data) : hash_hmac_file('sha384', $data, $key));
+    }
+
+    public static function sha512_equals(string $hash, string $data, string $key = ''): bool
+    {
+        return hash_equals($hash, self::sha512($data, $key));
     }
 
     /**
@@ -41,7 +66,12 @@ class Hash
      */
     public static function sha512(string $data, string $key = ''): string
     {
-        return strtoupper($key == '' ? hash('sha512', $data, false) : hash_hmac('sha512', $data, $key, false));
+        return strtoupper($key == '' ? hash('sha512', $data) : hash_hmac('sha512', $data, $key));
+    }
+
+    public static function sha512_file_equals(string $hash, string $data, string $key = ''): bool
+    {
+        return hash_equals($hash, self::sha512_file($data, $key));
     }
 
     /**
@@ -49,6 +79,6 @@ class Hash
      */
     public static function sha512_file(string $data, string $key = ''): string
     {
-        return strtoupper($key == '' ? hash_file('sha512', $data, false) : hash_hmac_file('sha512', $data, $key, false));
+        return strtoupper($key == '' ? hash_file('sha512', $data) : hash_hmac_file('sha512', $data, $key));
     }
 }
