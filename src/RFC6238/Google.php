6de72fc2f2fced3eb0cd7db6896c6bfcac4fe55d
jyxjjj <773933146@qq.com>
2024-01-08 17:05:37 +0800

    RFC6238

--- src/RFC6238/Google.php
+++ src/RFC6238/Google.php
@@ -2,14 +2,19 @@
 
 namespace DESMG\RFC6238;
 
-class Google
+use DESMG\RFC3548\Base32;
+
+readonly class Google
 {
-    private static array $lut = [
-        'A' => 0, 'B' => 1, 'C' => 2, 'D' => 3, 'E' => 4, 'F' => 5, 'G' => 6, 'H' => 7,
-        'I' => 8, 'J' => 9, 'K' => 10, 'L' => 11, 'M' => 12, 'N' => 13, 'O' => 14, 'P' => 15,
-        'Q' => 16, 'R' => 17, 'S' => 18, 'T' => 19, 'U' => 20, 'V' => 21, 'W' => 22, 'X' => 23,
-        'Y' => 24, 'Z' => 25, '2' => 26, '3' => 27, '4' => 28, '5' => 29, '6' => 30, '7' => 31,
-    ];
+    private function __construct(
+        private string $key = '',
+        private string $algorithm = 'sha1',
+        private int    $length = 6,
+        private int    $window = 2,
+        public int     $timestamp = 0,
+    )
+    {
+    }
 
     public static function generateKey(): string
     {
@@ -22,61 +27,51 @@ class Google
         return $s;
     }
 
-    public static function verify(string $key, string $pass): bool
+    public static function instance(string $key, string $algorithm = 'sha1', int $length = 6, int $window = 2, int $period = 30): ?self
     {
-        $timeStamp = self::getTimestamp();
-        for ($ts = $timeStamp - 2; $ts <= $timeStamp + 2; $ts++) {
-            if (self::getPass($key, $ts) == $pass) {
+        if (strlen($key) != 32) {
+            return null;
+        }
+        if (!in_array($algorithm, ['sha1', 'sha256', 'sha512'], true)) {
+            return null;
+        }
+        if ($length != 6 && $length != 8) {
+            return null;
+        }
+        if ($window < 0 || $window > 2) {
+            return null;
+        }
+        if (!in_array($period, [30, 60], true)) {
+            return null;
+        }
+        return new self($key, $algorithm, $length, $window, (int)floor(microtime(true) / $period));
+    }
+
+    public function verifyHOTP(string $pass, int $counter = 0): bool
+    {
+        return $this->getPass($counter) == $pass;
+    }
+
+    public function getPass(?int $counter = null): string|false
+    {
+        $key = Base32::decode($this->key);
+        $bin_counter = pack('N*', 0) . pack('N*', $counter ?? $this->timestamp);
+        $hash = hash_hmac($this->algorithm, $bin_counter, $key, true);
+        $offset = ord($hash[strlen($hash) - 1]) & 0xF;
+        $temp = unpack('N', substr($hash, $offset, 4));
+        $temp = $temp[1] & 0x7FFFFFFF;
+        $hash = substr($temp, 0 - $this->length);
+        return str_pad($hash, $this->length, '0', STR_PAD_LEFT);
+    }
+
+    public function verifyTOTP(string $pass): bool
+    {
+        $timeStamp = $this->timestamp;
+        for ($ts = $timeStamp - $this->window; $ts <= $timeStamp + $this->window; $ts++) {
+            if ($this->getPass($ts) == $pass) {
                 return true;
             }
         }
         return false;
-    }
-
-    public static function getTimestamp(): int
-    {
-        return (int)floor(microtime(true) / 30);
-    }
-
-    public static function getPass(string $key, int $counter): string|false
-    {
-        if (strlen($key) < 16) {
-            return false;
-        }
-        $key = self::base32_decode($key);
-        $bin_counter = pack('N*', 0) . pack('N*', $counter);
-        $hash = hash_hmac('sha1', $bin_counter, $key, true);
-        $hash = self::truncate($hash);
-        return str_pad($hash, 6, '0', STR_PAD_LEFT);
-    }
-
-    private static function base32_decode(string $b32): string
-    {
-        $b32 = strtoupper($b32);
-        if (!preg_match('/^[ABCDEFGHIJKLMNOPQRSTUVWXYZ234567]+$/', $b32)) {
-            exit('Invalid characters in the base32 string.');
-        }
-        $l = strlen($b32);
-        $n = 0;
-        $j = 0;
-        $binary = '';
-        for ($i = 0; $i < $l; $i++) {
-            $n <<= 5;
-            $n += self::$lut[$b32[$i]];
-            $j += 5;
-            if ($j >= 8) {
-                $j -= 8;
-                $binary .= chr(($n & (0xFF << $j)) >> $j);
-            }
-        }
-        return $binary;
-    }
-
-    private static function truncate(string $hash): int
-    {
-        $offset = ord($hash[strlen($hash) - 1]) & 0xF;
-        $temp = unpack('N', substr($hash, $offset, 4));
-        $temp = $temp[1] & 0x7FFFFFFF;
-        return substr($temp, -6);
     }
 }
--- tests/Units/G2FATest.php
+++ tests/Units/G2FATest.php
@@ -10,10 +10,21 @@ class G2FATest
     {
         $key = Google::generateKey();
         echo $key . PHP_EOL;
-        $timeStamp = Google::getTimestamp();
-        $totp = Google::getPass($key, $timeStamp);
-        echo $totp . PHP_EOL;
-        $verify = Google::verify($key, $totp) ? 'success' : 'fail';
-        echo $verify . PHP_EOL;
+
+        $googles[] = Google::instance($key);
+        $googles[] = Google::instance($key, 'sha1', 6, 2, 60);
+        $googles[] = Google::instance($key, 'sha1', 6, 2, 60);
+        $googles[] = Google::instance($key, 'sha1', 8);
+        $googles[] = Google::instance($key, 'sha256');
+        $googles[] = Google::instance($key, 'sha256', 8);
+        $googles[] = Google::instance($key, 'sha512');
+        $googles[] = Google::instance($key, 'sha512', 8);
+
+        foreach ($googles as $google) {
+            $totp = $google->getPass();
+            echo $totp . PHP_EOL;
+            $verify = $google->verifyTOTP($totp) ? 'success' : 'fail';
+            echo $verify . PHP_EOL;
+        }
     }
 }
