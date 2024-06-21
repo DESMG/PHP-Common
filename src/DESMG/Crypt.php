b5316ffc6782a7e4cd6f920ed49c51135a0ecb5b
jyxjjj <773933146@qq.com>
2024-06-21 11:02:36 +0800

    fix name

--- src/DESMG/Crypt.php
+++ src/DESMG/Crypt.php
@@ -8,16 +8,16 @@ use Throwable;
 
 class Crypt
 {
-    public static function decrypt(string $payload, string $key): ?string
+    public static function decrypt(string $cipher, string $key): ?string
     {
         try {
             $key = hex2bin($key);
-            $payload = hex2bin($payload);
-            $iv = substr($payload, 0, 16);
-            $tag = substr($payload, 16, 16);
-            $payload = substr($payload, 32);
+            $cipher = hex2bin($cipher);
+            $iv = substr($cipher, 0, 16);
+            $tag = substr($cipher, 16, 16);
+            $cipher = substr($cipher, 32);
             $aad = hex2bin(Hash::sha512($iv));
-            $data = openssl_decrypt($payload, '2.16.840.1.101.3.4.1.46', $key, OPENSSL_RAW_DATA, $iv, $tag, $aad);
+            $data = openssl_decrypt($cipher, '2.16.840.1.101.3.4.1.46', $key, OPENSSL_RAW_DATA, $iv, $tag, $aad);
             if (is_string($data)) {
                 return $data;
             } else {
@@ -28,7 +28,7 @@ class Crypt
         }
     }
 
-    public static function encrypt(string $data, string $key): ?string
+    public static function encrypt(string $payload, string $key): ?string
     {
         if ((strtoupper($key) == $key) && ctype_xdigit($key) && strlen($key) === 64) {
             $key = hex2bin($key);
@@ -39,9 +39,9 @@ class Crypt
             $iv = openssl_random_pseudo_bytes(16); // 128 bits instead of 96 bits for more security
             $aad = hex2bin(Hash::sha512($iv));
             $tag = null;
-            $payload = openssl_encrypt($data, '2.16.840.1.101.3.4.1.46', $key, OPENSSL_RAW_DATA, $iv, $tag, $aad);
-            if (strlen($payload) > 0) {
-                return strtoupper(bin2hex($iv . $tag . $payload)); // 16 + 16 + {$payload} | payload length equals to $data
+            $cipher = openssl_encrypt($payload, '2.16.840.1.101.3.4.1.46', $key, OPENSSL_RAW_DATA, $iv, $tag, $aad);
+            if (strlen($cipher) > 0) {
+                return strtoupper(bin2hex($iv . $tag . $cipher)); // 16 + 16 + {$cipher} | cipher length equals to $data
             } else {
                 return null;
             }
